fcd5c453dba03330bbc59e295403e9669d8d228c
Nilesh Kumar Banara <nileshkumarbanara@gmail.com>
2024-06-14 16:50:05 +0530

    First commit

---
+++ src/compositePatterns/starPyramid.java
@@ -0,0 +1,40 @@
+package compositePatterns;
+
+import java.util.Scanner;
+
+public class starPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+        //methode 1
+
+        for(int i = 1; i <= n ; i++){
+            for(int j=1; j <= n-i;j++){
+                System.out.print("  ");
+            }
+            for(int k = 1; k<=(2*i)-1;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+            //methode 2
+
+        int nsp = n-1; //number of space
+        int nst = 1; //number of star
+
+        for(int i = 1; i <= n ; i++){
+            for(int j=1; j <= nsp;j++){
+                System.out.print("  ");
+            }
+            for(int k = 1; k<=nst;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            nsp--;
+            nst += 2;
+
+        }
+    }
+}
